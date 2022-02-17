3a01984a955a69e956e734f8371b24647c8d9013
me.ankit88@gmail.com <me.ankit88@gmail.com>
2022-02-17 02:08:24 -0500

    Move logic to service layer

---
+++ src/main/java/com/example/mongoDBWithSpring/service/GetItemDetailsService.java
@@ -0,0 +1,16 @@
+package com.example.mongoDBWithSpring.service;
+
+import com.example.mongoDBWithSpring.model.GroceryItem;
+
+import java.util.List;
+import java.util.Set;
+
+public interface GetItemDetailsService {
+
+    Set<String> getGroceryItemsNames();
+    void getGroceryItemByName(String name);
+    List<GroceryItem> getItemsByCategory(String category);
+    long findCountOfGroceryItems();
+    void updateItemQuantity(String name, float newQuantity);
+    void deleteGroceryItem(String id);
+}
